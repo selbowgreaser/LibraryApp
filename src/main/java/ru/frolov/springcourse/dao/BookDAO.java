ca2651c503e33a561e93e72cdfd2da3e987eeade
selbowgreaser <selbowgreaser@gmail.com>
2023-03-14 12:11:47 +0300

    Added dao methods for working with books

--- src/main/java/ru/frolov/springcourse/dao/BookDAO.java
+++ src/main/java/ru/frolov/springcourse/dao/BookDAO.java
@@ -21,51 +21,53 @@ public class BookDAO {
 
     public List<Book> getAllBooks() {
         return jdbcTemplate.query(
-                "SELECT b.id id, b.title title, a.id author_id, a.full_name author_full_name, b.year book_year, p.id person_id, p.full_name person_full_name, p.birth_year person_birth_year " +
+                "SELECT b.id id, b.title title, a.id author_id, a.full_name author_full_name, b.book_year book_year, p.id person_id, p.full_name person_full_name, p.birth_year person_birth_year " +
                 "FROM book b JOIN author a on b.author_id = a.id LEFT JOIN person p on p.id = b.person_id", new BookMapper());
     }
 
-    public Person getBookPageById(int id) {
+    public Book getBookById(int id) {
         return jdbcTemplate.queryForObject(
-                        "SELECT * FROM book WHERE id = ?",
-                        new BeanPropertyRowMapper<>(Person.class),
-                        id);
+                "SELECT b.id id, b.title title, a.id author_id, a.full_name author_full_name, b.book_year book_year, p.id person_id, p.full_name person_full_name, p.birth_year person_birth_year " +
+                        "FROM book b JOIN author a on b.author_id = a.id LEFT JOIN person p on p.id = b.person_id WHERE b.id = ?",
+                new BookMapper(),
+                id);
 
     }
 
-    public void createBookPage(Book book) {
+    public void addBook(Book book) {
         jdbcTemplate.update(
-                "INSERT INTO book(title, author_id, year) values (?, ?, ?)",
+                "INSERT INTO book(title, author_id, book_year) values (?, ?, ?)",
                 book.getTitle(),
                 book.getAuthorId(),
                 book.getYear());
     }
-//
-//    public void createPersonPage(Person person) {
-//        jdbcTemplate.update(
-//                "INSERT INTO person(full_name, birth_year) values (?, ?)",
-//                person.getFullName(),
-//                person.getBirthYear());
-//    }
-//
-//    public void updatePersonInfo(Person updatedPerson) {
-//        jdbcTemplate.update(
-//                "UPDATE person SET full_name = ?, birth_year = ? WHERE id = ?",
-//                updatedPerson.getFullName(),
-//                updatedPerson.getBirthYear(),
-//                updatedPerson.getId());
-//    }
-//
-//    public void deletePersonPage(int id) {
-//        jdbcTemplate.update(
-//                "DELETE FROM person WHERE id = ?", id
-//        );
-//    }
-//
-//    public List<Book> getPersonBooks(int id) {
-//        return jdbcTemplate.query(
-//                "SELECT b.id, b.title, a.full_name, b.year FROM person p JOIN book b ON p.id = b.person_id JOIN author a ON a.id = b.author_id WHERE p.id = ?",
-//                new BeanPropertyRowMapper<>(Book.class),
-//                id);
-//    }
+
+    public void updateBook(Book updatedBook) {
+        jdbcTemplate.update(
+                "UPDATE book SET title = ?, author_id = ?, book_year = ? WHERE id = ?",
+                updatedBook.getTitle(),
+                updatedBook.getAuthorId(),
+                updatedBook.getYear());
+    }
+
+    public void deleteBook(int id) {
+        jdbcTemplate.update(
+                "DELETE FROM person WHERE id = ?", id
+        );
+    }
+
+    public void borrowBook(int personId, int id) {
+        jdbcTemplate.update(
+                "UPDATE book SET person_id = ? WHERE id = ?",
+                personId,
+                id
+        );
+    }
+
+    public void releaseBook(int id) {
+        jdbcTemplate.update(
+                "UPDATE book SET person_id = null WHERE id = ?",
+                id
+        );
+    }
 }
