bfa18eaa3fb64ce9899c8ccb69f4e3c9e14fa161
selbowgreaser <selbowgreaser@gmail.com>
2023-03-14 12:11:19 +0300

    Added mappings for working with books

--- src/main/java/ru/frolov/springcourse/controllers/BookController.java
+++ src/main/java/ru/frolov/springcourse/controllers/BookController.java
@@ -8,8 +8,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.frolov.springcourse.dao.AuthorDAO;
 import ru.frolov.springcourse.dao.BookDAO;
+import ru.frolov.springcourse.dao.PersonDAO;
 import ru.frolov.springcourse.model.Author;
 import ru.frolov.springcourse.model.Book;
+import ru.frolov.springcourse.model.Person;
 
 @Controller
 @RequestMapping("/books")
@@ -17,17 +19,32 @@ public class BookController {
 
     private final BookDAO bookDAO;
     private final AuthorDAO authorDAO;
+    private final PersonDAO personDAO;
 
     @Autowired
-    public BookController(BookDAO bookDAO, AuthorDAO authorDAO) {
+    public BookController(BookDAO bookDAO, AuthorDAO authorDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
         this.authorDAO = authorDAO;
+        this.personDAO = personDAO;
     }
 
     @GetMapping
     public String getAllBooks(Model model) {
         model.addAttribute("books", bookDAO.getAllBooks());
         return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getBookById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.getBookById(id));
+        model.addAttribute("people", personDAO.getAllPeople());
+        return "books/book";
+    }
+
+    @GetMapping("/new")
+    public String getFormForCreateBook(@ModelAttribute("book") Book book, @ModelAttribute("author") Author author, Model model) {
+        model.addAttribute("authors", authorDAO.getAllAuthors());
+        return "books/new";
     }
 
     @PostMapping
@@ -38,20 +55,50 @@ public class BookController {
             return "books/new";
         }
 
-        bookDAO.createBookPage(book);
+        bookDAO.addBook(book);
 
         return "redirect:/books";
     }
 
-    @GetMapping("/{id}")
-    public String getBookById(@PathVariable("id") int id, Model model) {
-        model.addAttribute("book", bookDAO.getBookPageById(id));
-        return "books/book";
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable("id") int id) {
+        bookDAO.deleteBook(id);
+
+        return "redirect:/books";
     }
 
-    @GetMapping("/new")
-    public String getFormForCreateBook(@ModelAttribute("book") Book book, @ModelAttribute("author") Author author, Model model) {
+    @GetMapping("/{id}/edit")
+    public String getFormForUpdateBook(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.getBookById(id));
         model.addAttribute("authors", authorDAO.getAllAuthors());
-        return "books/new";
+
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateBook(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "books/edit";
+        }
+
+        bookDAO.updateBook(book);
+        return "redirect:/books";
+    }
+
+    @PatchMapping("/{id}/borrow")
+    public String borrowBook(@ModelAttribute("book") Book book, Model model, @PathVariable("id") int id) {
+
+        bookDAO.borrowBook(book.getPersonId(), id);
+        model.addAttribute("book", bookDAO.getBookById(id));
+        return "redirect:/books/{id}";
+    }
+
+    @PatchMapping("/{id}/release")
+    public String releaseBook(@PathVariable("id") int id, Model model) {
+
+        bookDAO.releaseBook(id);
+        model.addAttribute("book", bookDAO.getBookById(id));
+        return "redirect:/books/{id}";
     }
 }
+
